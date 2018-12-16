0568cd13f1b3ba63ae4125946d9534a8d1e6e416
p1eXu5 <v1adp1exu5@gmail.com>
2018-12-16 21:51:07 +0300

    -add RuntimeWorkSpeedBusinessContext

---
+++ Code/WorkSpeed.Data/BusinessContexts/RuntimeWorkSpeedBusinessContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkSpeed.Data.Models;
+
+namespace WorkSpeed.Data.BusinessContexts
+{
+    public class RuntimeWorkSpeedBusinessContext : IWorkSpeedBusinessContext
+    {
+        private readonly List< Product > _products = new List< Product >( 40_000 );
+
+
+        public Task< bool > HasProductsAsync ()
+        {
+            return Task.Factory.StartNew( () => false );
+        }
+    }
+}
