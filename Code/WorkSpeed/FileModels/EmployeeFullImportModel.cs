084067f9f194daa14d320903695dd3dd6b0a3297
p1eXu5 <v1adp1exu5@gmail.com>
2018-12-19 00:11:00 +0300

    -add EmployeeFullImportModel

---
+++ Code/WorkSpeed/FileModels/EmployeeFullImportModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkSpeed.Interfaces;
+
+namespace WorkSpeed.FileModels
+{
+    class EmployeeFullImportModel : EmployeeImportModel
+    {
+        public override object Convert ( IImportModelVisitor visitor )
+        {
+            return visitor.GetDbModel( this );
+        }
+    }
+}
