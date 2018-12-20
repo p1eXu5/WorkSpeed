5cfb9ae1870f7601c87f6dbf38e6b87021532ec7
p1eXu5 <v1adp1exu5@gmail.com>
2018-12-20 16:33:55 +0300

    -add StageDataTemplateSelector, EmployeeCheckDesign

---
+++ Code/WorkSpeed.DesktopClient/TemplateSelectors/StageDataTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WorkSpeed.DesktopClient.ViewModels;
+
+namespace WorkSpeed.DesktopClient.TemplateSelectors
+{
+    public class StageDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate ( object item, DependencyObject container )
+        {
+            if ( container is FrameworkElement element ) {
+                if ( item is ImportStageViewModel ) {
+                    return element.FindResource( "dt_ImportStage" ) as DataTemplate;
+                }
+            }
+
+            return null;
+        }
+    }
+}
