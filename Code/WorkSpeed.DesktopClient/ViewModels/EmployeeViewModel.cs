eb702ff08c5cbe2e632e706427e3101020bbb5a0
p1eXu5 <v1adp1exu5@gmail.com>
2018-12-21 16:21:31 +0300

    -add EmployeeCheckDesign

--- Code/WorkSpeed.DesktopClient/ViewModels/EmployeeViewModel.cs
+++ Code/WorkSpeed.DesktopClient/ViewModels/EmployeeViewModel.cs
@@ -4,16 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WorkSpeed.Data.Models;
+using WorkSpeed.MvvmBaseLibrary;
 
 namespace WorkSpeed.DesktopClient.ViewModels
 {
-    public class EmployeeViewModel
+    public class EmployeeViewModel : ViewModel
     {
+        private readonly Employee _employee;
+
         public EmployeeViewModel (Employee employee)
         {
-            Employee = employee ?? throw new ArgumentNullException(nameof(employee));
+            _employee = employee ?? throw new ArgumentNullException(nameof(employee));
         }
 
-        public Employee Employee { get; }
+        public Employee Employee => _employee;
+
+        public string Id => _employee.Id;
+
+        public Appointment Appointment
+        {
+            get => _employee.Appointment;
+            set {
+                _employee.Appointment = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Name => _employee.Name;
+
+        public bool IsActive
+        {
+            get => _employee.IsActive;
+            set {
+                _employee.IsActive = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Position Position
+        {
+            get => _employee.Position;
+            set {
+                _employee.Position = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Rank Rank
+        {
+            get => _employee.Rank;
+            set {
+                _employee.Rank = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsSmoker
+        {
+            get => _employee.IsSmoker;
+            set {
+                _employee.IsSmoker = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
---
+++ Code/WorkSpeed.DesktopClient/Views/Design/EmployeeViewModelCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkSpeed.Data.Models;
+using WorkSpeed.DesktopClient.ViewModels;
+
+namespace WorkSpeed.DesktopClient.Views.Design
+{
+    public class EmployeeViewModelCollection : ObservableCollection< EmployeeViewModel >
+    {
+        public EmployeeViewModelCollection ()
+        {
+
+            Add( new EmployeeViewModel( new Employee {
+                Id = "AR12345",
+                Name = "Ландин Михаил Вячеславович",
+                IsActive = true,
+            } ) );
+
+            Add( new EmployeeViewModel( new Employee
+            {
+                Id = "AR65420",
+                Name = "Наконечный Александр Владимирович",
+                IsActive = true,
+            } ) );
+
+            Add( new EmployeeViewModel( new Employee
+            {
+                Id = "BY12013",
+                Name = "Шведов Владимир Викторович",
+                IsActive = true,
+            } ) );
+
+            Add( new EmployeeViewModel( new Employee
+            {
+                Id = "AR02983",
+                Name = "Реуцкий Евгений Васильевич",
+                IsActive = true,
+            } ) );
+        }
+
+
+    }
+}
