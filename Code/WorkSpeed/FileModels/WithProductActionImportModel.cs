cf640e431812f0e81e136f3dd734cefa23d10c3e
p1eXu5 <v1adp1exu5@gmail.com>
2018-12-27 15:14:36 +0300

    -ref WithProductActionImportModel

--- Code/WorkSpeed/FileModels/WithProductActionImportModel.cs
+++ Code/WorkSpeed/FileModels/WithProductActionImportModel.cs
@@ -4,28 +4,29 @@ namespace WorkSpeed.FileModels
 {
     public abstract class WithProductActionImportModel : ActionImportModel
     {
-        [Header("Код товара")]
+        [ Header( "Код товара" ) ]
         public int ProductId { get; set; }
 
-        [Header("Товар")]
+        [ Header( "Товар" ) ]
         public string Product { get; set; }
 
-        [Header("Код родителя")]
-        [Header("Родитель1Код")]
+        [ Header( "Код родителя" ) ]
+        [ Header( "Код родителя1" ) ]
+        [ Header( "Родитель1Код" ) ]
         public int ImmadiateProductId { get; set; }
 
-        [Header("Родитель1")]
+        [ Header( "Родитель1" ) ]
         public string ImmadiateProduct { get; set; }
 
-        [Header("Код родителя 2")]
-        [Header( "Родитель2Код" )]
+        [ Header( "Код родителя 2" ) ]
+        [ Header( "Родитель2Код" ) ]
         public int SecondProductId { get; set; }
 
-        [Header("Родитель2")]
+        [ Header( "Родитель2" ) ]
         public string SecondProduct { get; set; }
 
-        [Header("Количество")]
-        [Header("ФактическоеКоличество")]
+        [ Header( "Количество" ) ]
+        [ Header( "ФактическоеКоличество" ) ]
         public int ProductQuantity { get; set; }
     }
 }
