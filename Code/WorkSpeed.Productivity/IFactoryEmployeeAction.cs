305465879cb0e250f84d9a4ec18c46b6ca015e95
p1eXu5 <v1adp1exu5@gmail.com>
2018-12-27 15:03:32 +0300

    -ref IFactoryEmployeeAction

--- Code/WorkSpeed.Productivity/IFactoryEmployeeAction.cs
+++ Code/WorkSpeed.Productivity/IFactoryEmployeeAction.cs
@@ -1,21 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using WorkSpeed.Data.Models;
 
 namespace WorkSpeed.Productivity
 {
     public interface IFactoryEmployeeAction
     {
-        IPauseBetweenActions PauseBetweenActions { get; }
-        ICategoryFilter CategoryFilter { get; }
-
         void AddAction ( EmployeeAction action );
+
         ProductivityEmployee GetProductivity ( Employee employee );
 
         void AddVariableBreak ( string name, TimeSpan breakDuration, DayPeriod dayPeriod );
+
         void AddFixedBreaks ( string name,
                               TimeSpan duration,
                               TimeSpan interval,
                               TimeSpan offset,
                               Predicate<Employee> predicate );
+
+        IEnumerable< Category > GetCategories ();
+        double GetThreshold ();
+        void SetThreshold ( double threshold );
     }
 }
