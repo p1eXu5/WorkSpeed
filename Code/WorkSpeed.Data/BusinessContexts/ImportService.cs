43cd6d3037c091491167ce4ecf5db2c369cb8dc0
First Second <v1adp1exu5@gmail.com>
2019-02-05 18:47:17 +0300

    -init ImportService

--- Code/WorkSpeed.Data/BusinessContexts/ImportService.cs
+++ Code/WorkSpeed.Data/BusinessContexts/ImportService.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WorkSpeed.Data.BusinessContexts.Contracts;
+using WorkSpeed.Data.DataContexts;
 
 namespace WorkSpeed.Data.BusinessContexts
 {
     public class ImportService : Service, IImportService
     {
+        public ImportService ( WorkSpeedDbContext dbContext ) : base( dbContext ) { }
+
+        public void ImportFromXlsxAsync ( string fileName, IProgress< (string, double) > progress )
+        {
+            throw new NotImplementedException();
+        }
     }
 }
