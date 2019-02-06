91fb81d2ef27a5cc67d506448daa6a860ed4df2a
p1eXu5 <v1adp1exu5@gmail.com>
2019-02-06 17:08:12 +0300

    --vpp

---
+++ Code/WorkSpeed.Data/DataContexts/WorkSpeedDbContextExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkSpeed.Data.Models;
+
+namespace WorkSpeed.Data.DataContexts
+{
+    public static class WorkSpeedDbContextExtensions
+    {
+        public static void Add< TEntity > ( this WorkSpeedDbContext dbContext, TEntity entity ) where TEntity : class, IEntity
+        {
+
+        }
+    }
+}
