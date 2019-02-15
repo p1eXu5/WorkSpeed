c3111fe370b863c77878404f5b553955bc4ea443
p1eXu5 <v1adp1exu5@gmail.com>
2019-02-15 11:41:49 +0300

    add entity collections into ReportSeervice

--- Code/WorkSpeed.Business/Contexts/ReportService.cs
+++ Code/WorkSpeed.Business/Contexts/ReportService.cs
@@ -13,16 +13,28 @@ namespace WorkSpeed.Business.Contexts
 {
     public class ReportService : Service
     {
+        private readonly ObservableCollection< Shift > _shifts;
+        private readonly ObservableCollection< ShortBreakSchedule > _shortBreakSchedules;
+
         public ReportService ( WorkSpeedDbContext dbContext ) : base( dbContext )
         {
             Appointments = _dbContext.Appointments.ToArray();
             Ranks = _dbContext.Ranks.ToArray();
             Positions = _dbContext.Positions.ToArray();
+
+            _shifts = new ObservableCollection< Shift >( _dbContext.Shifts );
+            Shifts = new ReadOnlyObservableCollection< Shift >( _shifts );
+
+            _shortBreakSchedules = new ObservableCollection< ShortBreakSchedule >( _dbContext.ShortBreakSchedules );
+            ShortBreakSchedules = new ReadOnlyObservableCollection< ShortBreakSchedule >( _shortBreakSchedules );
         }
 
         public Appointment[] Appointments { get; }
         public Rank[] Ranks { get; }
         public Position[] Positions { get; }
+
+        public ReadOnlyObservableCollection< Shift > Shifts { get; set; }
+        public ReadOnlyObservableCollection< ShortBreakSchedule > ShortBreakSchedules { get; set; }
 
         public ShiftGrouping[] ShiftGrouping { get; private set; }
 
