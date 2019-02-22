e00f688594aca8b66ac801e4c03e75283c49e9dd
First Second <v1adp1exu5@gmail.com>
2019-02-22 15:58:18 +0300

    WorkSpeedDbContextExtensionsTests.cs Add

---
+++ Tests/WorkSpeed.Data.Tests/Context/ReportService/UnitTests/WorkSpeedDbContextExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WorkSpeed.Data.Tests.Context.ReportService.UnitTests
+{
+    [ TestFixture ]
+    public class WorkSpeedDbContextExtensionsTests
+    {
+        [ SetUp ]
+        public void SetupCulture ()
+        {
+
+        }
+
+        [ TearDown ]
+        public void CloseConnection ()
+        {
+
+        }
+
+        [ Test ]
+        public void GetEmployeeActions_ReturnsExpected ()
+        {
+
+        }
+    }
+}
